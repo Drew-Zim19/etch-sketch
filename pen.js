70188675b4d07129e6ad621a9a5c1169f8893b7f
Drew Zimmerman <drewjacobzimmerman@gmail.com>
2023-05-27 13:36:08 -0400

    Add grid creator, basic black coloring pad, clear button

--- pen.js
+++ pen.js
@@ -12,11 +12,43 @@ Algorithm breakdown
 const body = document.body;
 let pixelValue = '';
 
+//function below grabs input from the form to get number of pixels
+
 document.getElementById("pixelButton").addEventListener("click", () => {
 pixelValue = document.getElementById("pixelsForm").value;
 changeGridPixels(pixelValue);
 });
 
+//create the grid variable to manipulate for the sketch pad
+const grid = document.querySelector('.grid-container');
+
+
+//function changeGridPixels changes the number of pixels in the grid
+
 function changeGridPixels (pixelValue){
-    console.log(pixelValue);
+    let square = pixelValue;
+    /*grid.setAttribute('style', `grid-template-columns: repeat(${square}, 1fr);`);
+    grid.setAttribute('style', `grid-template-rows: repeat(${square}, 1fr);`);
+    */
+    grid.setAttribute('style', `grid-template-columns: repeat(${square}, 1fr); grid-template-rows: repeat(${square}, 1fr);`);
+
+   for (let i = 0; i < square * square; i++){
+        const div = document.createElement('div');
+        div.classList.add('gridSquare');
+        div.addEventListener('mouseover', (event) => {
+            event.target.style.backgroundColor = 'black';
+    });
+    grid.appendChild(div);
+   }
 }
+
+//clear button to remove all color and make divs white
+
+document.getElementById('clearButton').addEventListener('click', () => {
+        let nodeList = document.querySelectorAll(".gridSquare");
+        for(let i = 0; i < nodeList.length; i++){
+            nodeList[i].style.backgroundColor = 'white';
+        }
+        
+});
+
